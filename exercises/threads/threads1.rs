774e7fc5a96017f34062593542ca5c0944315f63
z <qqqqwer1213@163.com>
2025-12-23 18:05:48 +0800

    2

--- exercises/threads/threads1.rs
+++ exercises/threads/threads1.rs
@@ -6,9 +6,7 @@
 // return values into a vector.
 //
 // Execute `rustlings hint threads1` or use the `hint` watch subcommand for a
-// hint.
 
-// I AM NOT DONE
 
 use std::thread;
 use std::time::{Duration, Instant};
@@ -27,6 +25,9 @@ fn main() {
     let mut results: Vec<u128> = vec![];
     for handle in handles {
         // TODO: a struct is returned from thread::spawn, can you use it?
+                let elapsed_ms = handle.join().unwrap();
+        results.push(elapsed_ms);
+        
     }
 
     if results.len() != 10 {
